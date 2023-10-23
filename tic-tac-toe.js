a2b599136c354c4a33b8780e125a629b19fd2b62
Kristian Vaz <vaz.kristian@gmail.com>
2023-10-22 19:57:15 -0600

    Added Excercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     let squares = document.querySelectorAll('#board div');
-    squares.forEach(square => {
+    let gameState = Array(9).fill(null);
+    let currentPlayer = 'X';
+  
+    squares.forEach((square, index) => {
       square.classList.add('square');
+      square.addEventListener('click', () => {
+        if (gameState[index] === null) {
+          gameState[index] = currentPlayer;
+          square.textContent = currentPlayer;
+          square.classList.add(currentPlayer);
+          currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        }
+      });
     });
   });
